110dd158aaad7aa6fadab4fe2b20b0e274c10044
Neal Beeken <nbbeeken@gmail.com>
2021-04-01 20:19:19 -0400

    🎬

---
+++ scripts/server.js
@@ -0,0 +1,52 @@
+//@ts-check
+import { readFile } from 'fs/promises'
+import http from 'http'
+import path from 'path'
+import { on } from 'events'
+
+const server = http.createServer()
+server.listen(8080, 'localhost', 128, () => console.log('Listening on http://localhost:8080'))
+
+const DEFAULT_MIME = 'text/plain'
+
+const MIME_TYPE = new Map([
+	['js', 'application/javascript'],
+	['wasm', 'application/wasm'],
+	['css', 'text/css'],
+	['map', 'application/json'],
+	['json', 'application/json'],
+	['html', 'text/html'],
+])
+
+async function main() {
+	for await (const [request, response] of on(server, 'request')) {
+		const url = new URL(request.url, `http://${request.headers.host}`)
+
+		console.log(request.method, url.pathname)
+
+		try {
+			if (url.pathname === '/') {
+				response.setHeader('content-type', 'text/html')
+				response.end(await readFile(path.join('public', 'index.html')), 'utf8')
+				continue
+			} else {
+				const fileData = await readFile(path.join('public', url.pathname.slice(1)))
+
+				// Get the extension
+				const fileParts = url.pathname.split('.')
+				const fileExtension = fileParts[fileParts.length - 1]
+
+				response.setHeader('content-type', MIME_TYPE.get(fileExtension) ?? DEFAULT_MIME)
+				response.end(fileData)
+			}
+		} catch (error) {
+			response.statusCode = 500
+			response.end(`Bad Request: ${error}`, 'utf8')
+		}
+	}
+}
+
+main()
+	.then(() => console.log('end'))
+	.catch((e) => console.log(e))
+	.finally(() => server.close())
